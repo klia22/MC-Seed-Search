35495afd06e119023125ef17ff9cccfd724aa743
MC-dev-klia <klia2022@hotmail.com>
2026-04-03 11:25:19 -0700

    Delete run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM MC-Seed-Search Launcher for Windows
-REM Run this batch file to start the Minecraft Bedrock Structure Seed Searcher
-REM Requires Python 3.12+ to be installed and in PATH
-
-cd /d "%~dp0"
-python MC_SeedSearch.py %*
-pause
